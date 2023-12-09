2849af96a1d67ed42f650057f601b4962de66e0d
unknown <brannongarrett77@gmail.com>
2023-12-09 18:13:39 +0100

    Added new files and updated the code

--- factorial.c
+++ factorial.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include "cs50.h"
 
 int factorial(int number);
 
 int main(void)
 {
     // Prompt user for an integer
+    int n = get_int("Type a number: ");
+    printf("%i\n", factorial(n));
 }
 
 int factorial(int number)
