abf3af69159eef8888ec4297a19a99b0617f5b4c
unknown <brannongarrett77@gmail.com>
2023-12-09 18:10:28 +0100

    First commit

---
+++ factorial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int factorial(int number);
+
+int main(void)
+{
+    // Prompt user for an integer
+}
+
+int factorial(int number)
+{
+    // Return the factorial of the number
+}
